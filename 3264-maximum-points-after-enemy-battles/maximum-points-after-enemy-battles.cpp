84e2eb82779c8423d3b91b625044b33b30ad7e21
vanshsharma <129143208+vansh147@users.noreply.github.com>
2024-07-07 09:00:38 +0530

    Time: 119 ms (25.00%) | Memory: 76 MB (75.00%) - LeetSync

--- 3264-maximum-points-after-enemy-battles/maximum-points-after-enemy-battles.cpp
+++ 3264-maximum-points-after-enemy-battles/maximum-points-after-enemy-battles.cpp
@@ -4,20 +4,15 @@ public:
         long long count =0;
         sort(e.begin(),e.end());
        long long int n = e.size();
-       long long int l =0 ;
-        long long int r = n-1;
-        if(c>=e[0]){
-            count++;
-            c -= e[0];
-        }
-        else{
-            return count;
-        }
+      
        long long int sum =0;
+       if(c<e[0]){
+        return count;
+       }
         for(int i=1;i<n;i++){
             sum += e[i];
         }
-        sum = sum +c;
+        sum = sum + c;
         count += sum/e[0];
 
         return count;
